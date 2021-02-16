6699ef6bcc756cb20ea41c8f3152983ad0cac10d
Xenans <xenansgaming@gmail.com>
2021-02-15 20:42:20 -0800

    Added filtering for mentions to reduce spam

--- commands/say.js
+++ commands/say.js
@@ -3,12 +3,19 @@ module.exports = {
 	name: 'say',
 	description: `Usage: \`${prefix}say <message>\`\n
 	Repeats a given message. Please try not to abuse this command.`,
-	execute(message, args) {
+	execute(message, args, client) {
 		if (!args.length) {
 			return message.channel.send(`Please include a message!`)
 		}
 		concat = ""
 		for (arg of args) {
+			const matches = arg.match(/^<@!?(\d+)>$/);
+			if (matches)
+			{
+				concat += " " + "@" + client.users.cache.get(matches[1]).tag
+				continue;
+			}
+
 			concat += " " + arg
 		}
 		message.channel.send(concat);
