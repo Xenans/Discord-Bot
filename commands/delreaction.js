e27aa3dc4c0040b9daa12384b2c25bbb994b8507
Xenans <xenansgaming@gmail.com>
2021-11-28 02:02:50 -0500

    Added ability to delete reactions from the database

---
+++ commands/delreaction.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const { prefix } = require('../config.json');
+const mongoClient = require('../helpers/mongoClient.js');
+
+module.exports = {
+    name: 'delreaction',
+    description: `Placeholder text lol`,
+    async execute(message, args, client) {
+        for (let arg of args) {
+            mongoClient.db('reactions').collection(message.guild.id).deleteOne({ key: arg })
+            return message.channel.send(`Deleted all reactions for the key "${arg}""`)
+        }
+    },
+};
